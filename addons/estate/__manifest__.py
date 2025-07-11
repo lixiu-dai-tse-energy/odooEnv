95f2dfd4e52564ae85946c5e8a5e24baf60a24ec
lixiu-dai-tse-energy <lixiu.dai@tse.energy>
2025-07-11 14:14:38 +0200

    suivi de tutorial jusquà menu

---
+++ addons/estate/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{ 'name': "Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Lixiu",
+    'category': 'Category',
+    'application': True,
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    #'data': [
+   #     'views/mymodule_view.xml',
+    #],
+    # data files containing optionally loaded demonstration data
+   # 'demo': [
+       # 'demo/demo_data.xml',
+  #  ],
+  'data':
+      [
+'security/ir.model.access.csv',
+'views/estate_property_views.xml',
+'views/estate_menus.xml',
+  ]
+
+}
---
+++ addons/estate/models/estate.py
@@ -0,0 +1,24 @@
+from odoo import models,fields
+
+class Estate(models.Model):
+    _name="estate.property"
+    _description = "Estate Property"
+    #_order = "sequence"
+
+    name = fields.Char('Programme Name', required=True, translate=True)
+    description = fields.Char('Description', required=False)
+    expected_price = fields.Float(string="Expected Price", required = True)
+    postcode = fields.Char('Poste Code : ', required = True, translate = True)
+    date_avaibility = fields.Date(string='Date avaibility : ', copy=False, default=fields.Date.today())
+    selling_price = fields.Float('Selling price : ', required = False, readonly=True, copy=False)
+    bedrooms = fields.Integer("Number of bedrooms : ", required = False,default = 2)
+    living_area = fields.Integer('Living area : ')
+    facades = fields.Integer('Living area : ')
+    garage = fields.Integer('Number of garage : ')
+    garden = fields.Boolean("Is there a garden ? ")
+    garden_area = fields.Integer("Garden area : ")
+    active = fields.Boolean("Is there a active ? ", default = False, active=False)
+    state = fields.Selection([("New", "New"), ("Offer Received", "Offer Received"), ("Offer accepted", "Offer accepted"),("Sold", "Sold"),("Cancelled", "Cancelled")], copy=False, required=True, default=("New"))
+    garden_orientation = fields.Selection( [("south","south"), ("north","north"), ("west","west"), ("east","east")])
+
+
